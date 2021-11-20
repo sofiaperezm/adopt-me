459396cf92e6c35687da904c7f03ff14e02b4f6f
sofiaperezm <sofi.prz.m@gmail.com>
2021-11-20 02:32:47 -0500

    added the link component within the router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
@@ -28,8 +28,12 @@ import Details from "./Details";
 const App = () => {
   return (
     <div>
-      <h1>Adopt me!</h1>
       <Router>
+        <header>
+          <Link to="/">
+            <h1>Adopt me!</h1>
+          </Link>
+        </header>
         <Switch>
           <Route path="/details/:id">
             <Details />
