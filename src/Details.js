d0d0817eb3b47a43c716cd01ffb664951c93086b
sofiaperezm <sofi.prz.m@gmail.com>
2021-11-23 15:48:58 -0500

    added carousel component

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
+import Carousel from "./Carousel";
 
 class Details extends Component {
   state = { loading: true };
@@ -33,10 +34,12 @@ class Details extends Component {
     if (this.state.loading) {
       return <h2>loading... </h2>;
     }
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
 
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
